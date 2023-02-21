f37032c826a808df8275a69cea55b94504304579
selimtan <stanoncloud@gmail.com>
2023-02-21 18:04:52 +0300

    .

--- forms-react/src/components/InputSwitch/InputSwitchRenderer.tsx
+++ forms-react/src/components/InputSwitch/InputSwitchRenderer.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/css";
 import { InputMask } from 'primereact';
+import Switch from "react-switch";
 
 import React, { Fragment } from "react";
 import { is } from '@tuval/core';
@@ -20,7 +21,52 @@ function InputSwitchRenderer({ control }: IControlProperties) {
     `;
 
     return (
-        <InputSwitch checked={control.vp_Checked} onChange={(e) => is.function(control.vp_OnChange) ? control.vp_OnChange(e.value) : void 0} />
+        <Switch
+            checked={control.vp_Checked}
+            onChange={(e) => is.function(control.vp_OnChange) ? control.vp_OnChange(e) : void 0}
+            handleDiameter={30}
+            offColor="#F8F9FA"
+            onColor="#E14818"
+            offHandleColor="#FFF"
+            onHandleColor="#FFF"
+            height={40}
+            width={120}
+            borderRadius={0}
+            activeBoxShadow="0px 0px 1px 2px #fffc35"
+            uncheckedIcon={
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100%",
+                        fontSize: 15,
+                        color: "orange",
+                        paddingRight: 2
+                    }}
+                >
+                    Inactive
+                </div>
+            }
+            checkedIcon={
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100%",
+                        fontSize: 15,
+                        color: "orange",
+                        paddingRight: 2
+                    }}
+                >
+                    Active
+                </div>
+            }
+           
+            id="small-radius-switch"
+        />
+        /*   <InputSwitch checked={control.vp_Checked} onChange={(e) => is.function(control.vp_OnChange) ? control.vp_OnChange(e.value) : void 0} /> */
     )
 
 }
