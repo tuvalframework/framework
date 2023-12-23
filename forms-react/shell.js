387d8b0ad6e9de4d63fc4f64f26a82a86488a710
selimtan <stanoncloud@gmail.com>
2023-12-23 16:30:14 +0300

    .

--- forms-react/shell.js
+++ forms-react/shell.js
@@ -7,7 +7,8 @@ if (shell.exec('npm run buildweb').code !== 0) {
 //shell.cp('-Rf', './dist/index.js', '../../../realmocean/new-enterprise-bios/node_modules/@tuval/forms');
 
 shell.cp('-Rf', './dist/index.js', '../../../realmocean/realm-runtime/src/portal/static/bios/tuval-forms.js');
-shell.cp('-Rf', './dist/index.js', '../../../realmocean/realmocean-console/src/portal/static/tuval-forms.js');
+shell.cp('-Rf', './dist/index.js', '../../../realmocean_new/realmocean/app/realmocean/bios/tuval-forms.js');
 
 shell.cp('-Rf', './dist/index.d.ts', '../../../realmocean/realmocean-console-bios/node_modules/@tuval/forms');
-shell.cp('-Rf', './dist/index.js', '../../celmino_new/web-app/app/realm/bios/tuval-forms.js');
+shell.cp('-Rf', './dist/index.js', '../../../celmino/web-app/public/bios/tuval-forms.js');
+shell.cp('-Rf', './dist/index.js', '../../../celmino_new/web-app/app/realm/bios/tuval-forms.js');
