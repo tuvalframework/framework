deef23d3361ca06d34f9aba31eebd9706392fc56
selimtan <stanoncloud@gmail.com>
2024-01-18 13:32:47 +0300

    .

--- core/shell.js
+++ core/shell.js
@@ -8,15 +8,3 @@ if (shell.exec('npm run bundle').code !== 0) {
     shell.exit(1);
 }
 
-//shell.cp('-Rf', './dist/*', '../diagram/node_modules/@tuval/core');
-shell.cp('-Rf', './dist/*', '../core-graphics/node_modules/@tuval/core');
-shell.cp('-Rf', './dist/*', '../coreplus/node_modules/@tuval/core');
-shell.cp('-Rf', './dist/*', '../graphics/node_modules/@tuval/core');
-shell.cp('-Rf', './dist/*', '../forms/node_modules/@tuval/core');
-//shell.cp('-Rf', './dist/*', '../winforms/node_modules/@tuval/core');
-//shell.cp('-Rf', './dist/*', '../gui/node_modules/@tuval/core');
-//shell.cp('-Rf', './dist/*', '../gui/node_modules/@tuval/core');
-
-shell.cp('-Rf', './dist/*', '../../Tuval_Test_v2/node_modules/@tuval/core');
-shell.cp('-Rf', './dist/*', '../../../DemoApp/node_modules/@tuval/core');
-//shell.cp('-Rf', './dist/tuval-core-wp.js', '../../Tuval_Test_v_1/dist');
