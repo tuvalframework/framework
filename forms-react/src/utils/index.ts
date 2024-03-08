d80c3e08bb749ae4e559924b3257a000700ecdbe
selimtan <stanoncloud@gmail.com>
2024-03-08 08:53:16 +0300

    .

--- forms-react/src/utils/index.ts
+++ forms-react/src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './throttle'
 export * from './debounce'
+export * from './urlFriendly'
---
+++ forms-react/src/utils/urlFriendly.ts
@@ -0,0 +1,9 @@
+import {is} from '@tuval/core';
+
+export function urlFriendly(value) {
+    if (is.string(value)) {
+        return value == undefined ? '' : value.replace(/[^a-z0-9_]+/gi, '-').replace(/^-|-$/g, '').toLowerCase()
+    } else {
+        return '';
+    }
+}
