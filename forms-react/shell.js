e5ae9287752cbf22304020a5391ca2f1c70b4a81
selimtan <stanoncloud@gmail.com>
2024-03-25 00:53:00 +0300

    .

--- forms-react/shell.js
+++ forms-react/shell.js
@@ -4,15 +4,8 @@ if (shell.exec('npm run buildweb').code !== 0) {
     shell.exit(1);
 }
 
-
-//shell.cp('-Rf', './dist/index.js', '../../../realmocean/realm-runtime/src/portal/static/bios/tuval-forms.js');
 shell.cp('-Rf', './dist/index.js', '/Users/selimtan/Organizations-New/realmocean/runtime/app/realmocean/bios/tuval-forms.js');
 
-//shell.cp('-Rf', './dist/index.js', '../../../realmocean_new/runtime/app/realmocean/bios/tuval-forms.js');
-
-/* shell.cp('-Rf', './dist/index.d.ts', '../../../realmocean/realmocean-console-bios/node_modules/@tuval/forms');
-shell.cp('-Rf', './dist/index.js', '../../../celmino/web-app/public/bios/tuval-forms.js');
-shell.cp('-Rf', './dist/index.js', '../../../celmino_new/web-app/app/realm/bios/tuval-forms.js'); */
 
 shell.echo(`Exposing to docker`);
 shell.exec(`docker cp  ./dist/index.js  realmocean-dev:/usr/src/code/app/realmocean/bios/tuval-forms.js`);
